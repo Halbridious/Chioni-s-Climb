25df477db437b1e86f1e2af5468339de18946246
Jacob Hamberg <hamberg.jacob@gmail.com>
2019-06-02 01:54:31 -0400

    First physics
    +movement: players can now move using standard input in a strafe style
    
    + Look around: players can look around with the mouse
    
    + SpringJoint experiments: began playing with spring joints and their use as grappling hook tools.

---
+++ ChionisClimb/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+
+    [SerializeField]
+    //Holds a reference to the player camera
+    private Camera cam;
+
+    [SerializeField]
+    //Holds a reference to the players body
+    private Rigidbody body;
+
+    [SerializeField]
+    //the player's movement speed
+    private float moveSpeed = 10f;
+
+    [SerializeField]
+    //players mouse sensativity
+    private float lookSense = 3f;
+
+    [SerializeField]
+    //Reflects whether or not a player is currently grappling
+    private bool isGrappling = false;
+
+    [SerializeField]
+    //relfects whether or not a player is jumping
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        DoMovement();
+        DoTurn();
+    }
+
+    //Update movement
+    void DoMovement()
+    {
+        //get input values
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        //create the base vectors
+        Vector3 _horMove = transform.right * _xMove;
+        Vector3 _forMove = transform.forward * _zMove;
+        //total up the vectors
+        Vector3 _velocity = (_horMove + _forMove).normalized * moveSpeed;
+        //Move as long as there is a velocity input
+        if (_velocity != Vector3.zero)
+        {
+            body.MovePosition(transform.position + _velocity * Time.deltaTime);
+        }        
+    }
+
+    //Update rotations (based on mouse) for the horizontal yaw, not pitch
+    void DoTurn()
+    {
+        //use the yaw rotation to turn the body of the player
+        float _yawRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yawRotation, 0f) * lookSense;
+        body.MoveRotation(body.rotation * Quaternion.Euler(_rotation));
+        
+        if (cam != null)//can't rotate a camera that's not there
+        {
+            //use the pitch rotation to angle the camera
+            float _pitchRotation = Input.GetAxisRaw("Mouse Y");
+
+            Vector3 _camRotate = new Vector3(_pitchRotation, 0f, 0f) * lookSense;
+
+            Vector3 _newCam = cam.transform.localRotation.eulerAngles - _camRotate;
+            cam.transform.localEulerAngles = _newCam;
+        }
+    }
+
+}
