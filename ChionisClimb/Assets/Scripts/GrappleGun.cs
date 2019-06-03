93186ffc4f74d63360544f198cb76684b02c0a8f
Jacob Hamberg <hamberg.jacob@gmail.com>
2019-06-03 02:32:23 -0400

    Sprint 2
    +Raycast spawning items
    + UI

---
+++ ChionisClimb/Assets/Scripts/GrappleGun.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleGun : MonoBehaviour
+{
+    [SerializeField]
+    //Stores a reference to a camera
+    Camera cam;
+
+    [SerializeField]
+    //Stores a reference to the grapple prefab
+    GameObject grapplePrefab;
+
+    [SerializeField]
+    //Stores the active grapple
+    GameObject grapple;
+
+    [SerializeField]
+    //Range of grapple
+    float grappleRange = 10f;
+
+    [SerializeField]
+    //Stores mouse state
+    bool mouse1Pressed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the left mouse button.  Will need to hold down to maintain grapple.
+        if(Input.GetMouseButton(0))
+        {
+            mouse1Pressed = true;
+            if (!grapple)
+            {
+                ShootRay();
+            }
+            else
+            {
+                Debug.Log("GrappleExists!");
+            }
+            
+        }
+        else
+        {
+            mouse1Pressed = false;
+            Destroy(grapple);
+        }
+    }
+
+    //Project a ray to determine shot
+    void ShootRay()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, cam.transform.forward, out hit, grappleRange))
+        {
+            Debug.Log("Hit!");
+            grapple = Instantiate(grapplePrefab, hit.point, Quaternion.identity, transform);
+            if (grapple.GetComponent<SpringJoint>())
+            {
+                Debug.Log("Hit2");
+                grapple.GetComponent<SpringJoint>().connectedBody = GetComponent<Rigidbody>();
+            }
+        }
+        else
+        {
+            
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Chionis Climb
+Chioni's Climb
+
+///Story So Far///
+
+When Chioni's gramdother passed away, she took the old ways with her.  Now, Chioni's tribe has lost it's way, and she doesn't know what to do.  Recalling Nana's stories by the fire, Chioni recalls an old favorite, that described speaking to the spirits of the dead and the Gods from the top of Pagetónas, the great glacier.  Determined to restore the heart of her tibe, she takes trusty grapple and piton and prepares to tackle Pagetonas and seek guidance from the past.
+
+///Mechanics///
+Players control Chioni's movement using WASD, and control her vision with the mouse.  They can jump with the Spacebar, fire a grapple with the left click, and fire the piton tool with the right click.  Chioni's Climb is a 3D puzzle platformer.
